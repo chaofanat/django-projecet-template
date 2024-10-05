6ac75ee2b4dd8b796add5a983b0623fbfe652869
chaofan <chaofanat@qq.com>
2024-10-05 19:51:17 +0800

    详细配置了数据库和电子邮件服务，并调整了用户认证和会话管理的设置。
    - 注释掉了原有的MySQL数据库配置，准备从环境变量中读取数据库信息
    - 添加了详细的django-allauth配置，涉及用户认证、会话管理和邮件验证等方面
    - 从环境变量中读取Redis服务器URL，用于配置缓存
    - 删除了原有的EMAIL_BACKEND配置，直接配置了QQ邮箱作为邮件服务器
    - 在CACHES配置中添加了默认缓存设置，使用Redis作为缓存后端

--- MainConfig/settings.py
+++ MainConfig/settings.py
@@ -110,6 +110,23 @@ DATABASES = {
     }
 }
 
+# MYSQL_SERVER_URL = os.environ.get('MYSQL_SERVER_URL',"127.0.0.1")
+# MYSQL_DATABASE_NAME = os.environ.get('MYSQL_DATABASE_NAME',"chaofanonline")
+# MYSQL_USER_NAME = os.environ.get('MYSQL_USER_NAME',"SUPERVISOR")
+# MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD',"441608")
+# MYSQL_PORT_NUMBER = os.environ.get('MYSQL_PORT_NUMBER',"3306")
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': MYSQL_DATABASE_NAME,
+#         'USER': MYSQL_USER_NAME,
+#         'PASSWORD': MYSQL_PASSWORD,
+#         'HOST': MYSQL_SERVER_URL,
+#         'PORT': MYSQL_PORT_NUMBER
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -148,8 +165,7 @@ SOCIALACCOUNT_PROVIDERS = {
     },
 }
 
-#邮箱认证登录后跳转
-LOGIN_REDIRECT_URL = "/accounts/profile/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
@@ -184,16 +200,75 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# django-allauth其他配置
+# 用户登录
+ACCOUNT_AUTHENTICATION_METHOD = "username_email" # 邮箱或用户名登录
+# ACCOUNT_LOGIN_BY_CODE_ENABLED (default: False)
+# “Login by email” offers an alternative method of logging in. Instead of entering an email address and accompanying password, the user only enters the email address. Then, a one-time code is sent to that email address which allows the user to login. This method is often referred to as “Magic Code Login”. This setting controls whether or not this method of logging in is enabled.
+ACCOUNT_EMAIL_VERIFICATION =  "optional" # 注册邮箱验证, 可选值 "强制(mandatory)"、 "可选(optional)" 或 "否(none)" 之一
+ACCOUNT_EMAIL_REQUIRED = True # 设置用户注册的时候必须填写邮箱地址
+ACCOUNT_LOGOUT_ON_GET = False           # 用户登出(需要确认)
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3 # 邮箱确认邮件的截止日期(天数)
+ACCOUNT_USERNAME_MIN_LENGTH = 4 # 用户名最小长度
+ACCOUNT_USERNAME_BLACKLIST = ['admin'] # 用户名黑名单
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True # 登录是否自动跳转
+ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = True # 修改密码后是否自动登出
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = False # 重置密码后是否自动登录
+ACCOUNT_SESSION_REMEMBER = None # 控制会话的生命周期。设置为无以询问用户（“还记得我吗？”），False 表示不记得，True 表示始终记住。
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = False # 注册时是否需要确认两次邮箱
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True # 注册时是否需要确认两次密码
+ACCOUNT_UNIQUE_EMAIL = True # 邮箱是否唯一
+SOCIALACCOUNT_AUTO_SIGNUP = True # 社交账号是否自动注册,使用从社交账号提供者检索的字段(如用户名、邮件)来绕过注册表单
+ACCOUNT_CHANGE_EMAIL = False # 禁用（False）时，用户可以将一个或多个电子邮件地址（最多 ACCOUNT_MAX_EMAIL_ADDRESSES）添加到他们的帐户并自由管理这些电子邮件地址。启用（True）时，用户只能拥有一个电子邮件地址，他们可以通过添加临时的第二个电子邮件地址来更改该地址，该地址在验证后替换当前电子邮件地址。
+# ACCOUNT_MAX_EMAIL_ADDRESSES (default: None)
+# The maximum amount of email addresses a user can associate to his account. It is safe to change this setting for an already running project – it will not negatively affect users that already exceed the allowed amount. Note that if you set the maximum to 1, users will not be able to change their email address.
+ACCOUNT_EMAIL_NOTIFICATIONS = False # 启用（True）时，用户可以启用或禁用电子邮件通知(密码更改，登录地址变更等）。
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True # 将经过身份验证的用户在尝试访问登录 / 注册页面时重定向到 LOGIN_REDIRECT_URL。
+ACCOUNT_EMAIL_VERIFICATION_BY_CODE_ENABLED = False # 控制是通过跟踪电子邮件中的链接（False）还是通过输入代码（True）来执行电子邮件验证。
+
+ACCOUNT_SIGNUP_FORM_HONEYPOT_FIELD = "phone_number" # 为注册表单增加一个额外的不需要的字段，对于用户是隐藏的，但是当机器人输入此字段后，服务器将不会真的创建用户。
+# ACCOUNT_EMAIL_VERIFICATION_BY_CODE_MAX_ATTEMPTS (default: 3)
+# This setting controls the maximum number of attempts the user has at inputting a valid code.
+
+# ACCOUNT_EMAIL_VERIFICATION_BY_CODE_TIMEOUT (default: 900)
+# The code that is emailed has a limited life span. It expires this many seconds after which it was sent.
+
+# ACCOUNT_EMAIL_SUBJECT_PREFIX (default: "[Site] ")
+# Subject-line prefix to use for email messages sent. By default, the name of the current Site (django.contrib.sites) is used.
+
+# ACCOUNT_EMAIL_UNKNOWN_ACCOUNTS (default: True)
+# Configures whether password reset attempts for email addresses which do not have an account result in sending an email.
+
+# ACCOUNT_DEFAULT_HTTP_PROTOCOL (default: "http")
+# The default protocol used for when generating URLs, e.g. for the password forgotten procedure. Note that this is a default only – see the section on HTTPS for more information.
+
+# 登录后跳转
+LOGIN_REDIRECT_URL = "/accounts/profile/"
+# 登出后跳转
+ACCOUNT_LOGOUT_REDIRECT_URL = "/" 
+
+ACCOUNT_RATE_LIMITS = {
+    "change_password": "5/m/user",
+    "manage_email": "10/m/user",
+    "reset_password": "20/m/ip,5/m/key",
+    "reauthenticate": "10/m/user",
+    "reset_password_from_key": "20/m/ip",
+    "signup": "20/m/ip",
+    "login": "30/m/ip",
+    "login_failed": "10/m/ip,5/5m/key",
+    "confirm_email": "1/3m/key"
+}
 
 # email
 #邮件配置,需要去三方邮箱开启授权服务
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-ACCOUNT_EMAIL_VERIFICATION =  "optional" # 注册邮箱验证
 EMAIL_HOST = 'smtp.qq.com'  # 如果是 163 改成 smtp.163.com
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'chaofanat@qq.com'  # 发送邮件的邮箱帐号
@@ -204,9 +279,11 @@ EMAIL_USE_SSL = True   # 使用ssl
 # EMAIL_USE_TLS = False # 使用tls
 # EMAIL_USE_SSL 和 EMAIL_USE_TLS 是互斥的，即只能有一个为 True
 
+
+
+
 #从环境变量中读取服务网络
 REDIS_SERVER_URL = os.environ.get('REDIS_SERVER_URL',"127.0.0.1")
-
 
 # redis 配置
 CACHES = {
